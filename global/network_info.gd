fc31c264a48274e369ab6572bde2c1427174f83a
sroshc <shirzaysroshsafa@gmail.com>
2025-03-23 18:20:07 -0400

    Updated get_local_ip()

--- global/network_info.gd
+++ global/network_info.gd
@@ -8,10 +8,11 @@ extends Node
 @export var ip_address: String = "10.0.0.0"
 
 func get_local_ip() -> String:
-	var ip = ""
-	for address in IP.get_local_addresses():
-		if "." in address and not address.begins_with("127.") and not address.begins_with("169.254."):
-			if address.begins_with("192.168.") or address.begins_with("10.") or (address.begins_with("172.") and int(address.split(".")[1]) >= 16 and int(address.split(".")[1]) <= 31):
-				ip = address
-				break
-	return ip
+	var interfaces = IP.get_local_interfaces()
+	print(str(interfaces))
+	
+	for interface in interfaces:
+		if interface.name == "wlan0":
+			return  interface.addresses[0]
+	
+	return "(not found)"
